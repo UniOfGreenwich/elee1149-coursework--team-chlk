382368ad35f7aeb0c4f92b9594d1ae6e55092d6a
Kyle <kande294@ford.com>
2025-01-24 15:52:35 +0000

    Updating package name

--- src/main/java/com/fairshare/TestController.java
+++ src/main/java/com/fairshare/TestController.java
@@ -1,12 +1,7 @@
-package com.sprintboottest;
+package com.fairshare;
 
-import com.sprintboottest.entity.Customer;
-import com.sprintboottest.repository.CustomerRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 public class TestController {
--- src/main/java/com/fairshare/entity/Customer.java
+++ src/main/java/com/fairshare/entity/Customer.java
@@ -1,9 +1,7 @@
-package com.sprintboottest.entity;
+package com.fairshare.entity;
 
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
