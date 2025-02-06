9b8cb284459f27fc4c8ca419921e52ca09c11ddf
Kyle <kande294@ford.com>
2025-02-06 10:21:21 +0000

    tested

--- src/main/java/com/fairshare/repository/ExpenseRepository.java
+++ src/main/java/com/fairshare/repository/ExpenseRepository.java
@@ -4,5 +4,4 @@ import com.fairshare.entity.Expense;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
-    Expense saveExpense(Expense expense);
 }
