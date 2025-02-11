bee918fb7f722076d1d2ae6ba5bd0328fbf367e3
hamzak1406 <hkhan27@ford.com>
2025-02-11 09:45:08 +0000

    created login page component

---
+++ frontend/src/pages/login.jsx
@@ -0,0 +1,17 @@
+// Login Page
+import '../styles/login-page.css'
+import { Link } from 'react-router-dom'
+import { LoginForm } from "../components/login-form"
+
+export function Login() {
+    return (
+        <div className="form-page-wrapper">
+            <Link to='/'>
+                <img src={require('../assets/Fairshare-logo.png')} alt="logo in white" className='logo' />
+            </Link>
+
+            <LoginForm />
+        </div>
+
+    )
+}
