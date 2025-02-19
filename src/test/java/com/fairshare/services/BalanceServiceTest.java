3575a305aad3a861bcecdbfbc390eaef1e611f96
Kyle <kande294@ford.com>
2025-02-19 16:48:43 +0000

    Adding comments explaining testGetNetBalance - unit test

--- src/test/java/com/fairshare/services/BalanceServiceTest.java
+++ src/test/java/com/fairshare/services/BalanceServiceTest.java
@@ -28,19 +28,22 @@ class BalanceServiceTest {
 
     @Test
     void testGetNetBalance() {
-        // Initialise our variables to pass through our test
+        /* Create and initialise our variables that we need to pass through the test
+         this will match the parameters of the method you are testing. */
         String user1 = "user1";
         String user2 =  "user2";
-        Balance balance = new Balance();
-        balance.setUser1Id(user1);
-        balance.setUser2Id(user2);
-        balance.setNetAmount(100.0);
+        Balance balance = new Balance(); //Creat a new balance object
+        balance.setUser1Id(user1); //Set the user1Id field of the Balance object to user1
+        balance.setUser2Id(user2); // Ditto for user2
+        balance.setNetAmount(100.0); //Set the netAmount field of the Balance object to 100.0
 
-
+        //This mocks the behaviour of the balanceRepository.findByUsers method
         when(balanceRepository.findByUsers(user1, user2)).thenReturn(balance);
 
+        // Call the method we are testing and store it in a result variable
         Double result = balanceService.getNetBalance(user1, user2);
 
+        //Assert that the result is equal to the 100.0 we set intially
         assertEquals(100.0, result);
     }
 }
