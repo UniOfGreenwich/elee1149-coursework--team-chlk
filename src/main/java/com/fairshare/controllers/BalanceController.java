03c84e037186bb84f675cf1090cd56038854b6d8
Kyle <kande294@ford.com>
2025-02-20 11:59:39 +0000

    Updating balance controller to include service messages

--- src/main/java/com/fairshare/controllers/BalanceController.java
+++ src/main/java/com/fairshare/controllers/BalanceController.java
@@ -2,6 +2,8 @@ package com.fairshare.controllers;
 
 import com.fairshare.services.BalanceService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,17 +19,18 @@ public class BalanceController {
     private BalanceService balanceService;
 
     @PostMapping("/update")
-    public void updateBalance(@RequestParam String payerId, @RequestParam String payeeId, @RequestParam double amount) {
+    public ResponseEntity<String> updateBalance(@RequestParam String payerId, @RequestParam String payeeId, @RequestParam double amount) {
         balanceService.updateBalance(payerId, payeeId, amount);
+        return ResponseEntity.status(HttpStatus.OK).body("Balance updated successfully");
     }
 
-//    @GetMapping("/net")
-//    public Double getNetBalance(@RequestParam String user1, @RequestParam String user2){
-//        return balanceService.getNetBalance(user1, user2);
-//    }
-
     @GetMapping("/{user1}/{user2}")
-    public Double getNetBalance(@PathVariable String user1, @PathVariable String user2) {
-        return balanceService.getNetBalance(user1, user2);
+    public ResponseEntity<Object> getNetBalance(@PathVariable String user1, @PathVariable String user2) {
+        Double netBalance =  balanceService.getNetBalance(user1, user2);
+        if (netBalance != null) {
+            return ResponseEntity.status(HttpStatus.OK).body("Users net balance: " + netBalance);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Balance not found between users");
+        }
     }
 }
