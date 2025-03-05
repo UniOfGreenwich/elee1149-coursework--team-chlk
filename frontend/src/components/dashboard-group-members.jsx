246b401e7f2757272fbd44b66d98b93ba1517a69
hamzak1406 <hkhan27@ford.com>
2025-03-05 11:05:53 +0000

    fetching data using api call

--- frontend/src/components/dashboard-group-members.jsx
+++ frontend/src/components/dashboard-group-members.jsx
@@ -4,66 +4,71 @@ import { GroupMembersRow } from "./dashboard-group-members-row";
 
 export function GroupMembers() {
   //   fetching the data - currently not working
-  //   const [data, setData] = useState([]);
-  //   const [loading, setLoading] = useState(true);
-  //   const [error, setError] = useState(null);
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-  //   useEffect(() => {
-  //     fetch("http://localhost:8080/group/1/users")
-  //       .then((response) => {
-  //         if (!response.ok) {
-  //           throw new Error("Failed to fetch data");
-  //         }
-  //         return response.json();
-  //       })
-  //       .then((data) => {
-  //         setData(data);
-  //         setLoading(false);
-  //       })
-  //       .catch((error) => {
-  //         setError(error.message);
-  //         setLoading(false);
-  //       });
-  //   }, []);
+  useEffect(() => {
+    fetch("http://localhost:8080/group/1/users")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to fetch data");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error.message);
+        setLoading(false);
+      });
+  }, []);
 
-  //   if (loading) return <p>Loading...</p>;
-  //   if (error) return <p>Error: {error}</p>;
+  if (loading) {
+    return <p>Loading...</p>;
+  }
 
-  const data = [
-    {
-      userId: 5,
-      firstName: "Heather",
-      lastName: "Russell",
-      email: "Heather@gmail.com",
-      username: "HeatherR",
-      passwordHash: null,
-      password: "password789",
-      balance: 20,
-    },
+  if (error) {
+    console.log(data); //printing the data to the console
+    return <p>Error: {error}</p>;
+  }
 
-    {
-      userId: 1,
-      firstName: "Charu",
-      lastName: "Srivastava",
-      email: "Charu@email.com",
-      username: "charu_s",
-      passwordHash: null,
-      password: "password1",
-      balance: -30.5,
-    },
-    {
-      userId: 2,
-      firstName: "Kyle",
-      lastName: "Anderson",
-      email: "kyle@email.com",
-      username: "kyle_a",
-      passwordHash: null,
-      password: "password1",
-      balance: 15.0,
-    },
-  ];
+  // const data = [
+  //   {
+  //     userId: 5,
+  //     firstName: "Heather",
+  //     lastName: "Russell",
+  //     email: "Heather@gmail.com",
+  //     username: "HeatherR",
+  //     passwordHash: null,
+  //     password: "password789",
+  //     balance: 20,
+  //   },
 
-  // console.log(data); printing the data to the console
+  //   {
+  //     userId: 1,
+  //     firstName: "Charu",
+  //     lastName: "Srivastava",
+  //     email: "Charu@email.com",
+  //     username: "charu_s",
+  //     passwordHash: null,
+  //     password: "password1",
+  //     balance: -30.5,
+  //   },
+  //   {
+  //     userId: 2,
+  //     firstName: "Kyle",
+  //     lastName: "Anderson",
+  //     email: "kyle@email.com",
+  //     username: "kyle_a",
+  //     passwordHash: null,
+  //     password: "password1",
+  //     balance: 15.0,
+  //   },
+  // ];
+
 
   return (
     <div className="dashboard-grid-component">
