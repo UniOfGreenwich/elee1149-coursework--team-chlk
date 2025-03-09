2255dc326ddce32d8d6d395168e33ce4d1ea4f43
hamzak1406 <hkhan27@ford.com>
2025-03-09 16:43:27 +0000

    quick action buttons created

--- frontend/src/components/dashboard-quick-actions.jsx
+++ frontend/src/components/dashboard-quick-actions.jsx
@@ -1,7 +1,13 @@
+import "../styles/dashboard-quick-actions.css";
+
 export function QuickActions() {
   return (
     <div className="dashboard-grid-component">
       <h2 className="component-title">Quick Actions</h2>
+      <div className="button-container">
+        <button className="button add-expense">Add Expense</button>
+        <button className="button settle-payment">Settle Payment</button>
+      </div>
     </div>
   );
 }
