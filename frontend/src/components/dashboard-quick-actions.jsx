b77b6157f0a6e7f9c45bc2d20f4daf563871b497
hamzak1406 <hkhan27@ford.com>
2025-03-11 09:46:12 +0000

    updated quick actions component

--- frontend/src/components/dashboard-quick-actions.jsx
+++ frontend/src/components/dashboard-quick-actions.jsx
@@ -1,13 +1,40 @@
+//importing styles
 import "../styles/dashboard-quick-actions.css";
 
+import React, { useState } from "react";
+
+// importing components
+import AddExpense from "./add-expense";
+import SettlePayment from "./settle-payment";
+
 export function QuickActions() {
+  const [modalType, setModalType] = useState(null); // null means no modal is open
+
+  const openModal = (type) => setModalType(type);
+  const closeModal = () => setModalType(null);
+
   return (
     <div className="dashboard-grid-component">
       <h2 className="component-title">Quick Actions</h2>
       <div className="button-container">
-        <button className="button add-expense">Add Expense</button>
-        <button className="button settle-payment">Settle Payment</button>
+        <button
+          onClick={() => openModal("AddExpense")}
+          className="button add-expense">
+          Add Expense
+        </button>
+        <button
+          onClick={() => openModal("SettlePayment")}
+          className="button settle-payment">
+          Settle Payment
+        </button>
       </div>
+
+      {/* Conditionally Render Modals */}
+      {modalType === "AddExpense" && <AddExpense closeModal={closeModal} />}
+      {modalType === "SettlePayment" && (
+        <SettlePayment closeModal={closeModal} />
+      )}
     </div>
   );
 }
+
