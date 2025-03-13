317e5ec27064cfd3d6238303f203513142d90837
Kyle <kande294@ford.com>
2025-03-13 09:21:23 +0000

    Creating a group response class

---
+++ src/main/java/com/fairshare/Requests/GroupResponse.java
@@ -0,0 +1,39 @@
+package com.fairshare.Requests;
+
+import com.fairshare.entity.Group;
+
+public class GroupResponse {
+    private Group group;
+    private int numberOfUsers;
+    private double totalSpent;
+
+    public GroupResponse(Group group, int numberOfUsers, double totalSpent) {
+        this.group = group;
+        this.numberOfUsers = numberOfUsers;
+        this.totalSpent = totalSpent;
+    }
+
+    public Group getGroup() {
+        return group;
+    }
+
+    public void setGroup(Group group) {
+        this.group = group;
+    }
+
+    public int getNumberOfUsers() {
+        return numberOfUsers;
+    }
+
+    public void setNumberOfUsers(int numberOfUsers) {
+        this.numberOfUsers = numberOfUsers;
+    }
+
+    public double getTotalSpent() {
+        return totalSpent;
+    }
+
+    public void setTotalSpent(double totalSpent) {
+        this.totalSpent = totalSpent;
+    }
+}
