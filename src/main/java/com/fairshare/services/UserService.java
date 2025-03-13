a091d3f614c53dc9b39810bab7e712eaa4b9e3b8
Kyle <kande294@ford.com>
2025-03-13 09:20:55 +0000

    Updating user controller to add methods to userservice

--- src/main/java/com/fairshare/services/UserService.java
+++ src/main/java/com/fairshare/services/UserService.java
@@ -1,8 +1,11 @@
 package com.fairshare.services;
 
 import com.fairshare.Requests.CreateUserRequest;
+import com.fairshare.Requests.GroupResponse;
+import com.fairshare.entity.Expense;
 import com.fairshare.entity.Group;
 import com.fairshare.entity.User;
+import com.fairshare.repository.GroupRepository;
 import com.fairshare.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,11 +13,17 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 
+import static java.lang.Double.sum;
+
 @Service
 public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private GroupRepository groupRepository;
+    @Autowired
+    private ExpenseService expenseService;
 
     public User authenticateLogin (String email, String password) {
         User user = userRepository.findByEmail(email);
@@ -43,11 +52,29 @@ public class UserService {
         }
     }
 
-    public List<Group> getUsersGroups (Integer userId) {
+    public List<GroupResponse> getUsersGroups (Integer userId) {
         User user = userRepository.findById(userId).orElse(null);
         if (user != null) {
-            return new ArrayList<>(user.getGroups());
+            List<GroupResponse> groupResponses = new ArrayList<>();
+            for (Group group : user.getGroups()) {
+                int numberOfUsers = group.getUsers().size();
+                double totalSpent = calculateTotalMoneySpent(group);
+                groupResponses.add(new GroupResponse(group, numberOfUsers, totalSpent));
+            }
+            return groupResponses;
         }
         return null;
+    }
+
+    private double calculateTotalMoneySpent(Group group) {
+        return expenseService.getTotalExpensesByGroupId(group.getGroupId());
+    }
+
+    public int getNumOfUsersInGroup (Integer groupId) {
+        Group group = groupRepository.findById(groupId).orElse(null);
+        if (group != null) {
+            return group.getUsers().size();
+        }
+        return 0;
     }
 }
