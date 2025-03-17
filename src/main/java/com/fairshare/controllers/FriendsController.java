fd6f5de56896356c5e2807970277826dad0c48f7
Kyle <kande294@ford.com>
2025-03-17 14:15:25 +0000

    Adding imports and plan

--- src/main/java/com/fairshare/controllers/FriendsController.java
+++ src/main/java/com/fairshare/controllers/FriendsController.java
@@ -1,4 +1,24 @@
 package com.fairshare.controllers;
 
+import com.fairshare.services.FriendsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/friends")
 public class FriendsController {
+
+    @Autowired
+    private FriendsService friendsService;
+
+    //Methods:
+    //1. sendFriendRequest
+    //2. acceptFriendRequest
+    //3. declineFriendRequest
+    //4. getFriendsList
+    //5. getPendingFriendRequests
+    //6. areFriends
+
+
 }
