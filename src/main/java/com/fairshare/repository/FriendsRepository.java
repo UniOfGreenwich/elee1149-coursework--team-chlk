d26a170ca160ce373be565da1de85fe28ef75172
Kyle <kande294@ford.com>
2025-03-17 14:13:52 +0000

    Additional change to repository to enable better query

--- src/main/java/com/fairshare/repository/FriendsRepository.java
+++ src/main/java/com/fairshare/repository/FriendsRepository.java
@@ -2,6 +2,8 @@ package com.fairshare.repository;
 
 import com.fairshare.entity.Friends;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,10 +12,8 @@ import java.util.List;
 public interface FriendsRepository extends JpaRepository<Friends, Integer> {
     List<Friends> findByUserIdandStatus(Integer userId, Boolean status);
     List<Friends> findByFriendUserIdandStatus(Integer userId, Boolean status);
-    boolean existsByUserIdAndFriendUserId(Integer userId, Integer friendUserId, Boolean status);
 
+    @Query("SELECT CASE WHEN COUNT(f) > 0 THEN true ELSE false END FROM Friends f WHERE (f.userId = :userId AND f.friendUserId = :friendUserId AND f.status = true) OR (f.userId = :friendUserId AND f.friendUserId = :userId AND f.status = true)")
+    boolean existsFriendship(@Param("userId") Integer userId, @Param("friendUserId") Integer friendUserId);
 
 }
-
-
-
