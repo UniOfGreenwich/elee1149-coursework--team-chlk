651fd2ef2e071324409d76803fbad5a810b3174e
Charu <cs2726g@gre.ac.uk>
2025-03-24 12:56:41 +0000

    Added CreateExpenseResponse

---
+++ src/main/java/com/fairshare/Requests/CreateExpenseResponse.java
@@ -0,0 +1,38 @@
+package com.fairshare.Requests;
+
+public class CreateExpenseResponse {
+        private String message;
+        private Boolean success;
+        private Integer expenseId;
+
+        public CreateExpenseResponse(String message, Boolean success, Integer userId) {
+            this.message = message;
+            this.success = success;
+            this.expenseId = expenseId;
+        }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Boolean getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(Boolean success) {
+        this.success = success;
+    }
+
+    public Integer getExpenseId() {
+        return expenseId;
+    }
+
+    public void setExpenseId(Integer expenseId) {
+        this.expenseId = expenseId;
+    }
+}
+
