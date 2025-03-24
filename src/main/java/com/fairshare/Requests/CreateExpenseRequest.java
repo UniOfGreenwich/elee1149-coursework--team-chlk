697b25d67dcaeb9ef8a4826acee396715146f6f4
Charu <cs2726g@gre.ac.uk>
2025-03-24 12:53:25 +0000

    Added CreateExpenseRequest class

---
+++ src/main/java/com/fairshare/Requests/CreateExpenseRequest.java
@@ -0,0 +1,34 @@
+package com.fairshare.Requests;
+
+public class CreateExpenseRequest {
+
+        private String expenseName;
+        private Integer expenseId;
+        private Integer categoryId;
+
+    public String getExpenseName() {
+        return expenseName;
+    }
+
+    public void setExpenseName(String expenseName) {
+        this.expenseName = expenseName;
+    }
+
+    public Integer getExpenseId() {
+        return expenseId;
+    }
+
+    public void setExpenseId(Integer expenseId) {
+        this.expenseId = expenseId;
+    }
+
+    public Integer getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Integer categoryId) {
+        this.categoryId = categoryId;
+    }
+}
+
+
