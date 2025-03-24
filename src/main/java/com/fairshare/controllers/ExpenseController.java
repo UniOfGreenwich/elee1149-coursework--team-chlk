d3508d4da30a0c089f64c90e5569a2bc69c894d1
Charu <cs2726g@gre.ac.uk>
2025-03-24 14:40:05 +0000

    Modified Expense Controller to include new expense messages and added CreateExpenseResponse to it

--- src/main/java/com/fairshare/controllers/ExpenseController.java
+++ src/main/java/com/fairshare/controllers/ExpenseController.java
@@ -1,7 +1,12 @@
 package com.fairshare.controllers;
 
+import com.fairshare.Requests.CreateExpenseRequest;
+import com.fairshare.Requests.CreateExpenseResponse;
+import com.fairshare.Requests.CreateGroupRequest;
+import com.fairshare.Requests.CreateGroupResponse;
 import com.fairshare.entity.Expense;
 
+import com.fairshare.entity.Group;
 import com.fairshare.services.ExpenseService;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,12 +24,15 @@ public class ExpenseController {
     }
 
     @PostMapping("/add-expense")
-    public void addExpense(@RequestBody Expense expense, @RequestParam Integer payerId) {
-        if (expense.getCategoryId() == null) {
-            expense.setCategoryId(1); //set as deafult category
-        }
-        expenseService.addExpense(expense, payerId);
-
+    public CreateExpenseResponse addExpense(@RequestBody CreateExpenseRequest request){
+            Expense createdExpense = expenseService.addExpense(request);
+            if (createdExpense == null) {
+                return (new CreateExpenseResponse("User not found", false, null));
+            } else if (createdExpense.getExpenseName() != null && createdExpense.getExpenseName().equals("ExpenseExistsError")) {
+                return (new CreateExpenseResponse("An expense with this name already exists", false, null));
+            } else {
+                return (new CreateExpenseResponse("New expense created!", true, createdExpense.getExpenseId()));
+            }
     }
     @GetMapping("/all-expenses")
     public List<Expense> getAllExpenses(@RequestParam Integer groupId) {
