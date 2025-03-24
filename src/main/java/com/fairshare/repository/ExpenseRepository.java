9a8569b0c714113fcdc5ab980cdbf45e3caf1746
Charu <cs2726g@gre.ac.uk>
2025-03-24 13:09:11 +0000

    Modified ExpenseRepository

--- src/main/java/com/fairshare/repository/ExpenseRepository.java
+++ src/main/java/com/fairshare/repository/ExpenseRepository.java
@@ -2,14 +2,21 @@
 package com.fairshare.repository;
 
 import com.fairshare.entity.Expense;
+import com.fairshare.entity.Group;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
+
+    boolean existsByExpenseName(String expenseName);
+    boolean existsByExpenseId(Integer expenseId);
+    Optional<Expense> findByExpenseName(String expenseName);
+    Integer expenseId(Integer expenseId);
     List<Expense> findByGroupId(Integer groupId);
 
 }
