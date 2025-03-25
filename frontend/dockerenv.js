e973d00e95773ba4f14eb373266a6d6a14841b18
Kyle <kande294@ford.com>
2025-03-25 10:36:38 +0000

    Reverting back

--- frontend/dockerenv.js
+++
@@ -1,6 +0,0 @@
-const CODESPACE_NAME = process.env.CODESPACE_NAME || 'localhost';
-const BACKEND_URL = `https://${CODESPACE_NAME}-8080.app.github.dev`;
-
-module.exports = {
-    REACT_APP_BACKEND_URL: BACKEND_URL
-};
